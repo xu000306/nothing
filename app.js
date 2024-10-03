d5dea7c293745a124e59d7d401dd446e33fd6bbf
xu (Tom) weikai <xu000306@algonquinlive.com>
2024-10-03 13:25:27 -0400

    update the app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const app = {
-    init: () => {
-        console.log("app running");
-    }
+  one: 1,
+  two: 2,
+  init: () => {
+    console.log("app running");
+  },
 };
 
 setTimeout(app.init, 1000);
